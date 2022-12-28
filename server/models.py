142b392d0a048e6e33db3468e32e095f5377d81b
masha-astro <qur_x@mail.ru>
2022-12-28 19:30:04 +0300

    cut time_err and split time on time_0 and time_1

--- server/models.py
+++ server/models.py
@@ -6,15 +6,16 @@ from typing import List, Optional, Dict
 class Data:
     name: str
     
-    time: List[float]
-    time_error: List[float]
+    time_0: List[float]
+    time_1: List[float]
+    #time_error: List[float]
     flux_0_error: List[float]
     flux_1_error: List[float]
     flux_0: List[float]
     flux_1: List[float]
 
     time_aug: List[float]
-    time_error_aug: List[float]
+    #time_error_aug: List[float]
     flux_0_error_aug: List[float]
     flux_1_error_aug: List[float]
     flux_0_aug: List[float]
