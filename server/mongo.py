aaf4d01f81df46bb55cbe8fa6b5e8023ba4f4815
Matvey Volkov <m.b.volkov@yandex.ru>
2025-11-25 00:35:38 +0500

    escape query

--- server/mongo.py
+++ server/mongo.py
@@ -1,14 +1,12 @@
 import dataclasses
 import logging
-from typing import List, Union
-from typing import Optional
+import re
+from typing import List, Optional, Union
 
 import dacite
-
 import motor.motor_asyncio
 
-from server import models
-from server import config
+from server import config, models
 
 logger = logging.getLogger(__name__)
 
@@ -24,11 +22,11 @@ async def insert_data(document: Union[models.Data, models.DataVALC]) -> str:
     new_document = dataclasses.asdict(document)
 
     old_document = await collection.find_one(
-        {'name': document.name, 'type': document.type},
+        {"name": document.name, "type": document.type},
     )
     if old_document:
-        old_id = old_document['_id']
-        await collection.replace_one({'_id': old_id}, new_document)
+        old_id = old_document["_id"]
+        await collection.replace_one({"_id": old_id}, new_document)
         return str(old_id)
     else:
         result = await get_collection().insert_one(new_document)
@@ -36,30 +34,28 @@ async def insert_data(document: Union[models.Data, models.DataVALC]) -> str:
 
 
 async def get_data(
-        name: str, type: str,
+    name: str,
+    type: str,
 ) -> Optional[Union[models.Data, models.DataVALC]]:
-    data = await get_collection().find_one({'name': name, 'type': type})
+    data = await get_collection().find_one({"name": name, "type": type})
     if data is None:
         return None
 
-    if data.get('type') == 'FULU':
+    if data.get("type") == "FULU":
         return dacite.from_dict(models.Data, data)
-    elif data['type'] == 'VALC':
+    elif data["type"] == "VALC":
         return dacite.from_dict(models.DataVALC, data)
     else:
         return None
 
 
-async def get_all_names(type: str = 'FULU') -> List[str]:
-    return await get_collection().find({'type': type}).distinct('name')
+async def get_all_names(type: str = "FULU") -> List[str]:
+    return await get_collection().find({"type": type}).distinct("name")
 
 
 async def search_names(type: str, query: str) -> List[str]:
     collection = get_collection()
-    # Case-insensitive regex search for names containing the query
-    cursor = collection.find({
-        'type': type,
-        'name': {'$regex': query, '$options': 'i'}
-    })
-    names = await cursor.distinct('name')
+    escaped_query = re.escape(query)
+    cursor = collection.find({"type": type, "name": {"$regex": escaped_query, "$options": "i"}})
+    names = await cursor.distinct("name")
     return sorted(names)
