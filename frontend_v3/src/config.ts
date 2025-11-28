0de861120ed46503a80ca4b867dbf308f3dd2485
Matvey Volkov <m.b.volkov@yandex.ru>
2025-11-28 12:45:49 +0500

    fix api host

--- frontend_v3/src/config.ts
+++ frontend_v3/src/config.ts
@@ -1,4 +1,4 @@
 // export const HOST = process.env.REACT_APP_API_HOST || 'http://localhost:8000';
-export const HOST = process.env.REACT_APP_API_HOST || 'https://lc-dev.voxastro.org/api';
+export const HOST = process.env.REACT_APP_API_HOST || 'https://lc.voxastro.org/api';
 export const FULU_URL = process.env.REACT_APP_FULU_URL || 'https://lc-dev.voxastro.org/fulu.html';
 
